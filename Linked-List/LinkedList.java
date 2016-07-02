0a7f5bf4a711c7517ffb1515ca296aede13b60d0
Shailender Tiwari <shailendert@users.noreply.github.com>
2016-07-02 20:49:43 +0530

    Create LinkedList.java

---
+++ Linked-List/LinkedList.java
@@ -0,0 +1,34 @@
+/* This program shows the traversal of linked list element in java*/
+public class LinkedList{
+    
+    Node head;
+    static class Node{
+        int data ;
+        Node next;
+        /* Constructor of class Node */
+        Node(int d){
+            data = d;
+            next = null;
+        }
+    }
+    
+    public void printList(){
+        Node n = head ;
+        while(n != null){
+            System.out.println(n.data+"->");
+            n = n.next;
+        }
+    }
+    
+     public static void main(String []args){
+        LinkedList ll = new LinkedList();
+        ll.head = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+        
+        ll.head.next = second ;
+        second.next = third ;
+        
+        ll.printList();
+     }
+}
